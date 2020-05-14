8994affce43954cc0240489b60e9472fb4e6582c
易航 <stickyi@163.com>
2020-05-14 13:36:20 +0800

    独立出label显示文字和节点事件

--- src/components/org-tree/node.js
+++ src/components/org-tree/node.js
@@ -1,42 +1,83 @@
-// 判断是否叶子节点
+// 判断是否有子节点
 const isLeaf = (data, prop) => {
-  return !(Array.isArray(data[prop]) && data[prop].length > 0)
-}
+  return !(Array.isArray(data[prop]) && data[prop].length > 0);
+};
 
 // 创建 node 节点
 export const renderNode = (h, data, context) => {
-  const { props } = context
-  const cls = ['org-tree-node']
-  const childNodes = []
-  const children = data[props.props.children]
+  const { props } = context;
+  const cls = ['org-tree-node'];
+  const childNodes = [];
+  const children = data[props.props.children];
 
   if (isLeaf(data, props.props.children)) {
-    cls.push('is-leaf')
+    cls.push('is-leaf');
   } else if (props.collapsable && !data[props.props.expand]) {
-    cls.push('collapsed')
+    cls.push('collapsed');
   }
 
-  childNodes.push(renderLabel(h, data, context))
+  childNodes.push(renderLabel(h, data, context));
 
   if (!props.collapsable || data[props.props.expand]) {
-    childNodes.push(renderChildren(h, children, context))
+    childNodes.push(renderChildren(h, children, context));
   }
-
   return h('div', {
     domProps: {
       className: cls.join(' ')
     }
-  }, childNodes)
-}
+  }, childNodes);
+};
+
+// 创建节点文字显示div
+export const renderNodeInner = (h, data, { props, listeners }) => {
+  const label = data[props.props.label];
+
+  // event handlers
+  const clickHandler = listeners['on-node-click'];
+  const mouseOverHandler = listeners['on-node-mouseover'];
+  const mouseOutHandler = listeners['on-node-mouseout'];
+
+  let cls = ['org-tree-node-label-inner'];
+  let { labelWidth, labelClassName, selectedClassName, selectedKey } = props;
+
+  if (typeof labelWidth === 'number') {
+    labelWidth += 'px';
+  }
+
+  if (typeof labelClassName === 'function') {
+    labelClassName = labelClassName(data);
+  }
+
+  labelClassName && cls.push(labelClassName);
+
+  // add selected class and key from props
+  if (typeof selectedClassName === 'function') {
+    selectedClassName = selectedClassName(data);
+  }
+
+  selectedClassName && selectedKey && data[selectedKey] && cls.push(selectedClassName);
+
+  return h('div', {
+    domProps: {
+      className: cls.join(' '),
+      innerHTML: label
+    },
+    on: {
+      'click': e => clickHandler && clickHandler(e, data),
+      'mouseover': e => mouseOverHandler && mouseOverHandler(e, data),
+      'mouseout': e => mouseOutHandler && mouseOutHandler(e, data)
+    }
+  });
+};
 
 // 创建展开折叠按钮
 export const renderBtn = (h, data, { props, listeners }) => {
-  const expandHandler = listeners['on-expand']
+  const expandHandler = listeners['on-expand'];
 
-  let cls = ['org-tree-node-btn']
+  let cls = ['org-tree-node-btn'];
 
   if (data[props.props.expand]) {
-    cls.push('expanded')
+    cls.push('expanded');
   }
 
   return h('span', {
@@ -44,93 +85,48 @@ export const renderBtn = (h, data, { props, listeners }) => {
       className: cls.join(' ')
     },
     on: {
-      click: e => expandHandler && expandHandler(e,data)
+      click: e => expandHandler && expandHandler(e, data)
     }
-  })
-}
+  });
+};
 
 // 创建 label 节点
 export const renderLabel = (h, data, context) => {
-  const { props, listeners } = context
-  const label = data[props.props.label]
-  const renderContent = props.renderContent
+  const { props } = context;
 
-  // event handlers
-  const clickHandler = listeners['on-node-click']
-  const mouseOverHandler = listeners['on-node-mouseover']
-  const mouseOutHandler = listeners['on-node-mouseout']
-
-  const childNodes = []
-  if (typeof renderContent === 'function') {
-    let vnode = renderContent(h, data)
-
-    vnode && childNodes.push(vnode)
-  } else {
-    childNodes.push(label)
-  }
-
+  const childNodes = [];
+  childNodes.push(renderNodeInner(h, data, context));
   if (props.collapsable && !isLeaf(data, props.props.children)) {
-    childNodes.push(renderBtn(h, data, context))
+    childNodes.push(renderBtn(h, data, context));
   }
-
-  const cls = ['org-tree-node-label-inner']
-  let { labelWidth, labelClassName, selectedClassName, selectedKey } = props
-
-  if (typeof labelWidth === 'number') {
-    labelWidth += 'px'
-  }
-
-  if (typeof labelClassName === 'function') {
-    labelClassName = labelClassName(data)
-  }
-
-  labelClassName && cls.push(labelClassName)
-
-  // add selected class and key from props
-  if (typeof selectedClassName === 'function') {
-    selectedClassName = selectedClassName(data)
-  }
-
-  selectedClassName && selectedKey && data[selectedKey] && cls.push(selectedClassName)
-
   return h('div', {
     domProps: {
       className: 'org-tree-node-label'
     }
-  }, [h('div', {
-    domProps: {
-      className: cls.join(' ')
-    },
-    style: { width: labelWidth },
-    on: {
-      'click': e => clickHandler && clickHandler(e, data),
-      'mouseover': e => mouseOverHandler && mouseOverHandler(e, data),
-      'mouseout': e => mouseOutHandler && mouseOutHandler(e, data)
-    }
-  }, childNodes)])
-}
+  }, childNodes);
+};
 
 // 创建 node 子节点
 export const renderChildren = (h, list, context) => {
   if (Array.isArray(list) && list.length) {
     const children = list.map(item => {
-      return renderNode(h, item, context)
-    })
+      return renderNode(h, item, context);
+    });
 
     return h('div', {
       domProps: {
         className: 'org-tree-node-children'
       }
-    }, children)
+    }, children);
   }
-  return ''
-}
+  return '';
+};
 
 export const render = (h, context) => {
-  const {props} = context
+  const { props } = context;
 
-  console.log(context.listeners)
-  return renderNode(h, props.data, context)
-}
+  console.log(context.listeners);
+  return renderNode(h, props.data, context);
+};
 
-export default render
+export default render;
